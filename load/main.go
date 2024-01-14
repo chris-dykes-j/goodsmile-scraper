86153950ee1d875527036643ce1d6f93a2bf0a7b
Christopher Dykes <chris.t.c.dykes@gmail.com>
2024-01-14 12:39:06 -0500

    Begin loading functions

--- load/main.go
+++ load/main.go
@@ -1,15 +1,59 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
 func main() {
     // Connect to database
+    conn, err := pgx.Connect(context.Background(), "connectionstring")
+    if err != nil {
+        log.Fatal(err)
+    }
 
     // Run init script
+    init, err := os.ReadFile("init.sql")
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    sql := string(init) 
+    _, err = conn.Exec(context.Background(), sql)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-    // 
+    // Read jsonl file, line by line
+    figure := Figure{}
+
+    // Create insert statements
+    err = insertFigure(figure, conn)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Commit transaction
+}
+
+func insertFigure(figure Figure, conn *pgx.Conn) error {
+    sql := createInsertScript(figure)
+    _, err := conn.Exec(context.Background(), sql)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return nil
+}
+
+func createInsertScript(figfigure Figure) string {
+    return ""
 }
 
 // Input Data Structs
-type OldFigure struct {
+type Figure struct {
 	English  Data `json:"en"`
 	Japanese Data `json:"ja"`
 	Chinese  Data `json:"zh"`
